bddadc5dc80faf63e038359436154a786f09ab52
wulin <wulin@dtstack.com>
2017-06-13 10:56:53 +0800

    新增yarn包管理

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router';
 import { Menu, Icon } from 'antd';
-import './assets/css/common.css';
+import './assets/css/common.less';
 
 class App extends Component {
   
